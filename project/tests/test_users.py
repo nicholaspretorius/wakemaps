63bcaa6ccc01ded9932c988a18fa0752c63361b1
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-07 09:30:57 +0200

    tests: Add tests for single user

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -1,5 +1,8 @@
 import json
 
+from project import db
+from project.api.models import User
+
 def test_add_user(test_app, test_database):
     client = test_app.test_client()
     res = client.post('/users', data=json.dumps({
@@ -21,7 +24,7 @@ def test_add_user_invalid_json(test_app, test_database):
     assert 'invalid payload' in data['message']
     assert 'fail' in data['status']
 
-def test_add_user_invalid_kson_keys(test_app, test_database):
+def test_add_user_invalid_json_keys(test_app, test_database):
     client = test_app.test_client()
     res = client.post('/users', data=json.dumps({
         'email': 'test@test.com'
@@ -54,4 +57,33 @@ def test_add_user_duplicate_email(test_app, test_database):
     assert resp.status_code == 400
     assert 'Sorry. That email already exists.' in data['message']
     assert 'fail' in data['status']
- 
+
+def test_get_user(test_app, test_database):
+    user = User(username="John", email="john@doe.com")
+    db.session.add(user)
+    db.session.commit()
+
+    client = test_app.test_client()
+    res = client.get(f'/users/{user.id}')
+    data = json.loads(res.data.decode())
+    assert res.status_code == 200
+    assert 'John' in data['data']['username']
+    assert 'john@doe.com' in data['data']['email']
+    assert 'success' in data['status']
+
+def test_single_user_no_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/blah')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'There is no user with that user_id' in data['message']
+    assert 'fail' in data['status']
+
+
+def test_single_user_incorrect_id(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/999')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'There is no user with that user_id' in data['message']
+    assert 'fail' in data['status']
