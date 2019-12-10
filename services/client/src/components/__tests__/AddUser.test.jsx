f8142d00f2ad00598bb9c3f77a99f70d49abf4de
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-10 09:52:08 +0200

    tests: Initial AddUser tests

---
+++ services/client/src/components/__tests__/AddUser.test.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, cleanup, getByLabelText, getByText } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import AddUser from "./../AddUser";
+
+afterEach(cleanup);
+
+it("renders", () => {
+  const { asFragment } = render(<AddUser username="" email="" />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders with default props", () => {
+  const { getByLabelText, getByText } = render(<AddUser usrname="" email="" />);
+
+  const usernameInput = getByLabelText("Username");
+  expect(usernameInput).toHaveAttribute("type", "text");
+  expect(usernameInput).toHaveAttribute("required");
+  expect(usernameInput).not.toHaveValue();
+
+  const emailInput = getByLabelText("Email");
+  expect(emailInput).toHaveAttribute("type", "email");
+  expect(emailInput).toHaveAttribute("required");
+  expect(emailInput).not.toHaveValue();
+
+  const buttonInput = getByText("Submit");
+  expect(buttonInput).toHaveValue("Submit");
+});
--- services/client/src/components/__tests__/usersList.test.jsx
+++ services/client/src/components/__tests__/usersList.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import UsersList from "../usersList";
+import UsersList from "../UsersList";
 
 afterEach(cleanup);
 
