5a501b5eb99a58009f0dd349bcd10d280d2e6631
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-10 08:13:47 +0200

    feat: Setup first API call

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-const App = () => {
-  return (
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-one-third">
-            <br />
-            <h1 className="title is-1">Users</h1>
-            <hr />
-            <br />
+class App extends Component {
+  getUsers() {
+    axios
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+      .then(res => console.log("Users: ", res))
+      .catch(e => console.error(e));
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br />
+              <h1 className="title is-1">Users</h1>
+              <hr />
+              <br />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
