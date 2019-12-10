a678888e5e92b92ba42eb84abf8839cf517000e1
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-10 15:44:34 +0200

    tests: JWT

--- services/users/project/tests/test_users_model.py
+++ services/users/project/tests/test_users_model.py
@@ -6,3 +6,9 @@ def test_passwords_are_random(test_app):
     user_one = add_user("justatest", "test@test.com", "greaterthaneight")
     user_two = add_user("justatest2", "test2@test.com", "greaterthaneight")
     assert user_one.password != user_two.password
+
+
+def test_encode_auth_token(test_app):
+    user = add_user("justatest", "test@test.com", "greaterthaneight")
+    auth_token = user.encode_auth_token(user.id)
+    assert isinstance(auth_token, bytes)
