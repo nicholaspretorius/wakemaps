afb0cb9c46c247a83e0802da2094dcd56d9753e3
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-12 11:20:19 +0200

    tests: Update Login/Register tests

--- services/client/src/components/__tests__/LoginForm.test.jsx
+++ services/client/src/components/__tests__/LoginForm.test.jsx
@@ -6,12 +6,31 @@ import LoginForm from "./../LoginForm";
 
 afterEach(cleanup);
 
+const props = {
+  handleLoginFormSubmit: () => true
+};
+
 it("renders properly", () => {
-  const { getByText } = render(<LoginForm />);
+  const { getByText } = render(<LoginForm {...props} />);
   expect(getByText("Login")).toHaveClass("title");
 });
 
+it("renders with the default props", () => {
+  const { getByLabelText, getByText } = render(<LoginForm {...props} />);
+
+  const emailInput = getByLabelText("Email");
+  expect(emailInput).toHaveAttribute("type", "email");
+  expect(emailInput).not.toHaveValue();
+
+  const passwordInput = getByLabelText("Password");
+  expect(passwordInput).toHaveAttribute("type", "password");
+  expect(passwordInput).not.toHaveValue();
+
+  const buttonInput = getByText("Submit");
+  expect(buttonInput).toHaveValue("Submit");
+});
+
 it("renders", () => {
-  const { asFragment } = render(<LoginForm />);
+  const { asFragment } = render(<LoginForm {...props} />);
   expect(asFragment()).toMatchSnapshot();
 });
