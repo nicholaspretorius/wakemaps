b0424a58529fc6436ae186471494b9f927af3436
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-13 13:01:11 +0200

    chore: Prettier

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -129,7 +129,11 @@ class App extends Component {
     const { users, title } = this.state;
     return (
       <div>
-        <Nav title={title} logout={this.logoutUser} isAuthenticated={this.isAuthenticated} />
+        <Nav
+          title={title}
+          logout={this.logoutUser}
+          isAuthenticated={this.isAuthenticated}
+        />
         <section className="section">
           <div className="container">
             {this.state.messageType && this.state.messageText && (
@@ -179,7 +183,9 @@ class App extends Component {
                   <Route
                     exact
                     path="/status"
-                    render={() => <UserStatus isAuthenticated={this.isAuthenticated} />}
+                    render={() => (
+                      <UserStatus isAuthenticated={this.isAuthenticated} />
+                    )}
                   />
                 </Switch>
               </div>
